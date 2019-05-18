2a039d9bc5386937138b54ceabb083777e4bee60
Mohammed Tayor <m@tayor.net>
2019-05-18 04:30:06 +0000

    Update

--- gym_matlab/envs/matlab_env.py
+++ gym_matlab/envs/matlab_env.py
@@ -26,6 +26,9 @@ config = cfg_load.load(filepath)
 logging.config.dictConfig(config['LOGGING'])
 
 
+def matlab_function(x, y):
+    #cost/error
+    return np.power(x,2) + np.power(y, 2)
 
 class matlabEnv(gym.Env):
     """
@@ -38,6 +41,36 @@ class matlabEnv(gym.Env):
     def __init__(self):
         self.__version__ = "0.1.0"
         logging.info("matlabEnv - Version {}".format(self.__version__))
+        # General variables defining the environment
+        self.x = np.random.randint(1,10000)
+        self.y = np.random.randint(1,10000)
+        self.TOTAL_TIME_STEPS = 100
+
+
+        self.dxdy = [(dx, dy) for dx in [-0.1,0,0.1] for dy in [-0.1,0,0.1]]
+        # [-0.1, 0, 0.1] for x and [-0.1, 0, 0.1] for y
+        # (dx, dy) is [-0.1, 0,0.1]x[-0.1,0,0.1]
+
+        # Define what the agent can do
+        # card((dx, dy))=3*3=9
+        self.action_space = spaces.Discrete(9)
+
+        self.curr_step = -1
+
+
+        self.is_optimized = False
+
+        # Observation is the remaining time
+        low = np.array([0.0,  # remaining_tries
+                        ])
+        high = np.array([self.TOTAL_TIME_STEPS,  # remaining_tries
+                         ])
+        self.observation_space = spaces.Box(low, high, dtype=np.float32)
+
+        # Store what the agent tried
+        self.curr_episode = -1
+        self.action_episode_memory = []
+
 
     def step(self, action):
         """
@@ -69,14 +102,34 @@ class matlabEnv(gym.Env):
                  However, official evaluations of your agent are not allowed to
                  use this for learning.
         """
-        pass
+
+        if self.is_optimized:
+            raise RuntimeError("Episode is done")
+        self.curr_step += 1
+        self._take_action(action)
+        reward = self._get_reward()
+        ob = self._get_state()
+        return ob, reward, self.is_optimized, {}
         
     def _take_action(self, action):
-        pass
+        self.action_episode_memory[self.curr_episode].append(action)
+        #self.action_space.n is max possible actions (ie 9)
+        #dxdy= self.dxdy[int(self.action_space.n *random.random())]
+        dxdy= self.dxdy[int(action % self.action_space.n) ]
+        self.x += dxdy[0]*self.x
+        self.y += dxdy[1]*self.y
+        if matlab_function(self.x, self.y)==0:
+            self.is_optimized=True
+
+        #remaining_steps = self.TOTAL_TIME_STEPS - self.curr_step
+
 
     def _get_reward(self):
         """Reward is given for a sold matlab."""
-        pass
+        if self.is_optimized:
+            return 0.0
+        else:
+            return -matlab_function(self.x,self.y)
 
     def reset(self):
         """
@@ -86,13 +139,23 @@ class matlabEnv(gym.Env):
         -------
         observation (object): the initial observation of the space.
         """
-        pass
+        self.curr_step = -1
+        self.curr_episode += 1
+        self.action_episode_memory.append([])
+        self.is_optimized = False
+        self.x = np.random.randint(1,10000)
+        self.y = np.random.randint(1,10000)
+        return self._get_state()
 
     def _render(self, mode='human', close=False):
         pass
 
     def _get_state(self):
-        pass
+        """Get the observation."""
+        ob = [self.TOTAL_TIME_STEPS - self.curr_step]
+        return ob
 
     def seed(self, seed):
-        pass
+        random.seed(seed)
+        np.random.seed()
+
