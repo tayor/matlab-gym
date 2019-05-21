044ab1aae09c63d10a48db04c004e384ad1768c0
Mohammed Tayor <m@tayor.net>
2019-05-21 03:59:26 +0000

    Update matlab_env.py

--- gym_matlab/envs/matlab_env.py
+++ gym_matlab/envs/matlab_env.py
@@ -4,7 +4,7 @@
 """
 matlab simulation environment.
 
-Each episode is running an matlab simulation.
+Each episode is running a matlab simulation.
 """
 
 # core modules
@@ -31,13 +31,6 @@ def matlab_function(x, y):
     return np.power(x,2) + np.power(y, 2)
 
 class matlabEnv(gym.Env):
-    """
-    Define a simple matlab environment.
-
-    The environment defines which actions can be taken at which point and
-    when the agent receives which reward.
-    """
-
     def __init__(self):
         self.__version__ = "0.1.0"
         logging.info("matlabEnv - Version {}".format(self.__version__))
@@ -45,7 +38,6 @@ class matlabEnv(gym.Env):
         self.x = np.random.randint(1,10000)
         self.y = np.random.randint(1,10000)
         self.TOTAL_TIME_STEPS = 100
-
 
         self.dxdy = [(dx, dy) for dx in [-0.1,0,0.1] for dy in [-0.1,0,0.1]]
         # [-0.1, 0, 0.1] for x and [-0.1, 0, 0.1] for y
@@ -60,7 +52,7 @@ class matlabEnv(gym.Env):
 
         self.is_optimized = False
 
-        # Observation is the remaining time
+        # Observations are the current model input 
         low = np.array([0.0,  # remaining_tries
                         ])
         high = np.array([self.TOTAL_TIME_STEPS,  # remaining_tries
@@ -73,36 +65,6 @@ class matlabEnv(gym.Env):
 
 
     def step(self, action):
-        """
-        The agent takes a step in the environment.
-
-        Parameters
-        ----------
-        action : int
-
-        Returns
-        -------
-        ob, reward, episode_over, info : tuple
-            ob (object) :
-                an environment-specific object representing your observation of
-                the environment.
-            reward (float) :
-                amount of reward achieved by the previous action. The scale
-                varies between environments, but the goal is always to increase
-                your total reward.
-            episode_over (bool) :
-                whether it's time to reset the environment again. Most (but not
-                all) tasks are divided up into well-defined episodes, and done
-                being True indicates the episode has terminated. (For example,
-                perhaps the pole tipped too far, or you lost your last life.)
-            info (dict) :
-                 diagnostic information useful for debugging. It can sometimes
-                 be useful for learning (for example, it might contain the raw
-                 probabilities behind the environment's last state change).
-                 However, official evaluations of your agent are not allowed to
-                 use this for learning.
-        """
-
         if self.is_optimized:
             raise RuntimeError("Episode is done")
         self.curr_step += 1
@@ -129,22 +91,13 @@ class matlabEnv(gym.Env):
         if finish:
             self.is_optimized = True  # abuse this a bit
 
-
     def _get_reward(self):
-        """Reward is given for a sold matlab."""
         if self.is_optimized:
             return 0.0
         else:
             return -matlab_function(self.x,self.y)
 
     def reset(self):
-        """
-        Reset the state of the environment and returns an initial observation.
-
-        Returns
-        -------
-        observation (object): the initial observation of the space.
-        """
         self.curr_step = -1
         self.curr_episode += 1
         self.action_episode_memory.append([])
@@ -157,11 +110,9 @@ class matlabEnv(gym.Env):
         pass
 
     def _get_state(self):
-        """Get the observation."""
         ob = [self.TOTAL_TIME_STEPS - self.curr_step]
         return ob
 
     def seed(self, seed):
         random.seed(seed)
         np.random.seed()
-
